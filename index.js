aac9136d458c715de852556cbc1bba303418c72f
Ehsan <144959137+TssHack@users.noreply.github.com>
2025-04-24 02:20:35 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ytdlp = require('yt-dlp-exec');
+const ytdlp = require('yt-dlp-exec').create('./bin/yt-dlp');
 const app = express();
 const port = 3000;
 
